f1866a3590055020fc4d25ac5d46644bfdc14ceb
lengchang <448907872@qq.com>
2021-09-06 20:21:08 +0800

    提交704

---
+++ easy-704/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] nums = { };
+var target = 0;
+
+Console.WriteLine(Solution.Search(nums, target));
+
+public class Solution
+{
+    public static int Search(int[] nums, int target)
+    {
+        var position = nums.ToList().BinarySearch(target);
+
+        return position > 0 ? position : -1;
+    }
+}
+
+
+
